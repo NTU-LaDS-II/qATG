ba8cd78802b157117d22f105e99338beaeb93337
ChingHuanWang <85130393+ChingHuanWang@users.noreply.github.com>
2021-07-08 17:52:52 +0800

    ok

--- Gate.py
+++ Gate.py
@@ -9,23 +9,23 @@ import math
 
 
 def U3(parameter_list):
-    # theda=0, phi=0, lam=0
-    return np.array(
-            [[
-                np.cos(parameter_list[0] / 2),
-                -np.exp(1j * parameter_list[2]) * np.sin(parameter_list[0] / 2)
-            ],
-             [
-                 np.exp(1j * parameter_list[1]) * np.sin(parameter_list[0] / 2),
-                 np.exp(1j * (parameter_list[1] + parameter_list[2])) * np.cos(parameter_list[0] / 2)
-             ]],
-            dtype=complex)
+	# theda=0, phi=0, lam=0
+	return np.array(
+			[[
+				np.cos(parameter_list[0] / 2),
+				-np.exp(1j * parameter_list[2]) * np.sin(parameter_list[0] / 2)
+			],
+			 [
+				 np.exp(1j * parameter_list[1]) * np.sin(parameter_list[0] / 2),
+				 np.exp(1j * (parameter_list[1] + parameter_list[2])) * np.cos(parameter_list[0] / 2)
+			 ]],
+			dtype=complex)
 
 def CNOT():
-    return  np.array([[1,0,0,0],
-                      [0,0,0,1],
-                      [0,0,1,0],
-                      [0,1,0,0]])
+	return  np.array([[1,0,0,0],
+					  [0,0,0,1],
+					  [0,0,1,0],
+					  [0,1,0,0]])
 
 # print(
 # np.dot(np.dot( np.kron(U3(1,2,3), np.eye(2)), CNOT()), np.kron(np.eye(2), U3(4,5,6))) 
