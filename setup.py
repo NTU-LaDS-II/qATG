ba67703d2ecf583a499ca4537acf9d0f2c432727
CHENG-YUN HSIEH <55644510+William324-Hsieh@users.noreply.github.com>
2024-01-15 16:33:09 +0800

    update qiskit version

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     lab_email='lads427@gmail.com',
     license='BSD 3-clause',
     packages=['qatg'],
-    install_requires=['qiskit==0.39.0',
+    install_requires=['qiskit==0.44.3',
+					  'qiskit-aer==0.13.0', #https://github.com/Qiskit/qiskit-aer/issues/2007
 					  'pylatexenc>=2.10',
 					  'scipy>=1.7.1',
                       'ipympl>=0.9.2',
