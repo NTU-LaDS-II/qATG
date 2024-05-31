9a0d2968e83d65f20f056466b9c3a974c110dd8e
CK943177 <r11943177@ntu.edu.tw>
2024-05-31 12:29:30 +0800

    Format

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     lab_email='lads427@gmail.com',
     license='BSD 3-clause',
     packages=['qatg'],
-    install_requires=['qiskit==0.39.0',
+    install_requires=['qiskit==0.39',
 					  'pylatexenc>=2.10',
 					  'scipy>=1.7.1',
                       'ipympl>=0.9.2',
