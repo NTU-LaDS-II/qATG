021278ab5493bd178b517dcdc297a159ec7874c0
CK943177 <r11943177@ntu.edu.tw>
2024-06-04 17:15:14 +0800

    Setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='qatg',
-    description='An open-source quantum automatic test generator',
+    description='An open-source quantum automatic test generator, compatible with Qiskit.',
     url='https://github.com/NTU-LaDS-II/qATG',
     author='Chen-Hung Wu, Chin-Yang Jen, Ching-Huan Wang, Cheng-Yun Hsieh, Jiun-Yun Li, James Chien-Mo Li',
-    lab_email='lads427@gmail.com',
+    author_email='lads427@gmail.com',
     license='BSD 3-clause',
     packages=['qatg'],
     install_requires=['qiskit==0.44.3',
